416fbcfcef8f59b639d6d66ca2e7785651d774a2
Andy Yao <a_n_d_y_y_a_o@hotmail.com>
2016-02-02 01:49:29 +0000

    Added user model and validations, user creation.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
  
   resources :movies
+  resources :users, only: [:new, :create]
    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160202001235) do
+ActiveRecord::Schema.define(version: 20160202013115) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20160202001235) do
     t.datetime "release_date"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
