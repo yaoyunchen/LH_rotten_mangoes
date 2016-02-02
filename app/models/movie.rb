a142a9ba0d8642982ba8723d5d248cd72b5100a1
Andy Yao <a_n_d_y_y_a_o@hotmail.com>
2016-02-02 01:30:27 +0000

    Refactored movie forms and added validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,26 @@
 class Movie < ActiveRecord::Base
+  validates :title,
+    presence: true
+
+  validates :director,
+    presence: true
+
+  validates :runtime_in_minutes,
+    numericality: {
+      only_integer: true
+    }
+
+  validates :description,
+    presence: true
+
+  validates :poster_image_url,
+    presence: true
+
+  validate :release_date_is_past
+
+
+  protected
+    def release_date_is_past
+      errors.add(:release_date, "should be in the past") if release_date.present? && release_date > Date.today 
+    end
 end
