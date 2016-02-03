b5cc0734be9bea0c85008702380bedec7f202bcb
Andy Yao <a_n_d_y_y_a_o@hotmail.com>
2016-02-03 19:48:55 +0000

    Changed session resources to singular.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :sessions, only: [:new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
 
   resources :movies do
     resources :reviews, only: [:new, :create]
